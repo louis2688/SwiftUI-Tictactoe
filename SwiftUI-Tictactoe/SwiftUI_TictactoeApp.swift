d1427d0912964f8f911d5e057558136ad7d62658
louis2688 <louismadrigal26@gmail.com>
2023-05-06 00:28:30 +0800

    Initial Commit

---
+++ SwiftUI-Tictactoe/SwiftUI_TictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TictactoeApp.swift
+//  SwiftUI-Tictactoe
+//
+//  Created by dev on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
