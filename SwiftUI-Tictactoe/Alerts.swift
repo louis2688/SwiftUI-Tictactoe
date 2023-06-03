83f580888673b90df68ca08833aa100df7069213
louis2688 <louismadrigal26@gmail.com>
2023-06-03 15:56:36 +0800

    Add simple ai logic

---
+++ SwiftUI-Tictactoe/Alerts.swift
@@ -0,0 +1,21 @@
+//
+//  Alerts.swift
+//  SwiftUI-Tictactoe
+//
+//  Created by dev on 6/3/23.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    var title: Text
+    var message: Text
+    var buttonTitle: Text
+}
+
+struct AlertContext {
+   static let humanWin = AlertItem(title: Text("You Win"), message: Text("Your are so smart. You beat your own AI"), buttonTitle: Text("Hell Yeah"))
+   static let computerWin = AlertItem(title: Text("You Lost"), message: Text("You programmed a super AI"), buttonTitle: Text("Rematch"))
+   static let draw = AlertItem(title: Text("Draw"), message: Text("This is battle of genius"), buttonTitle: Text("Try Again"))
+}
