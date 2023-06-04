a7da2df35eff5fc07444f2d8d3e0d3eeaf0fedb5
louis2688 <louismadrigal26@gmail.com>
2023-06-04 15:37:17 +0800

    Change circle color

--- SwiftUI-Tictactoe/GameView.swift
+++ SwiftUI-Tictactoe/GameView.swift
@@ -62,7 +62,7 @@ struct GameCircleView: View {
     var proxy: GeometryProxy
     var body: some View {
         Circle()
-            .foregroundColor(.red).opacity(0.5)
+            .foregroundColor(.blue).opacity(0.5)
             .frame(width: proxy.size.width/3 - 15, height: proxy.size.width/3 - 15)
     }
 }
